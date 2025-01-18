ecbfda382b081fe55da21b18b9eb16d366426102
Steve Bachta <sbachta90@gmail.com>
2025-01-18 15:40:50 -0500

    Initial commit

---
+++ src/main/java/com/example/springrandom/SpringRandomApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRandomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRandomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrandom/SpringRandomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrandom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRandomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
